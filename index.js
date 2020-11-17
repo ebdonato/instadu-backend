8feaab56b3fc738212669548e11ef4663543348a
ebdonato <63134122+ebdonato@users.noreply.github.com>
2020-11-16 23:28:02 -0300

    Enviando notificações ao criar um novo post

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const admin = require('firebase-admin');
 
 let Busboy = require('busboy');
 
-let path = require('path')
-let os = require('os')
-let fs = require('fs')
-let UUID = require('uuid-v4')
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
+const UUID = require('uuid-v4')
+
+const webpush = require('web-push')
 
 //configuração - firebase
 
@@ -27,6 +29,14 @@ let bucket = admin.storage().bucket();
 
 const app = express()
 const port = 3000
+
+
+// configuração do web-push - enviar notificações
+webpush.setVapidDetails(
+    'mailto:example@yourdomain.org',
+    'BOt09vMIvG_JXilxKflPMM_FxJr1FgGhSAbZlLMxVGiH3alTfWo2jK5R6d4M6_TYZ8F9Oe40HYtslU2f4N4bd70',//vapidKeys.publicKey,
+    'fH350rrnNl0QgvFkdXafK2vqqtJ5MpvgFuPyofdy0rw' //vapidKeys.privateKey
+)
 
 // endpoint (rota?)
 
@@ -54,6 +64,7 @@ app.get('/posts', (request, response) => {
         })
 
 })
+
 // endpoint - createPost
 
 app.post('/createPost', (request, response) => {
@@ -111,16 +122,64 @@ app.post('/createPost', (request, response) => {
                 date: parseInt(fields.date),
                 imageUrl: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${uploadedFile.name}?alt=media&token=${uuid}`
             }).then(() => {
+                sendPushNotification()
                 response.send('Post Added: ' + fields.id)
             }).catch(console.error)
 
         }
+
+        function sendPushNotification() {
+
+            db.collection('subscriptions').get()
+                .then((snapshot) => {
+
+                    let subscriptions = []
+
+
+                    snapshot.forEach((doc) => {
+                        subscriptions.push(doc.data())
+                    })
+
+                    return subscriptions
+                }).then(subscriptions => {
+
+                    subscriptions.forEach(subscription => {
+
+                        const pushContent = {
+                            title: 'Nova postagem no Instadu!',
+                            body: 'Nova postagem adicionada! Wow!',
+                            openUrl: '/#/'
+                        }
+
+                        //TODO tratar as notificações expiradas
+                        webpush.sendNotification(subscription, JSON.stringify(pushContent)).catch(console.error)
+
+                    })
+
+                })
+
+        }
+
     })
 
     request.pipe(busboy)
 
 
 })
+
+// endpoint - createSubscription
+
+app.post('/createSubscription', (request, response) => {
+    response.set('Access-Control-Allow-Origin', '*')
+    db.collection('subscriptions').add(request.query).then(docReference => {
+        response.send({
+            message: 'Inscrição Adicionada',
+            postData: request.query
+        })
+    })
+
+})
+
 
 //escutando
 
